c2acf579cf2509851224ab9a979762d517b3fc6f
fallen.ink <fengzilijie@gmail.com>
2016-05-31 16:24:46 +0800

    +

---
+++ property-def.m
@@ -0,0 +1,11 @@
+// property define template
+// default
+//
+// IDECodeSnippetCompletionPrefix: property-def
+// IDECodeSnippetCompletionScopes: [Code Expression]
+// IDECodeSnippetIdentifier: 76A983B4-35B8-4B71-B4BF-2DE7E6B4CF78
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+
+@property (nonatomic, <#qualifier#>) <#type#> <#name#>;
